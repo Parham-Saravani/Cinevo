8bd3eb676b4ce682e919e58e9e7dd61487007bf7
Parham Saravani <parhamsaravani83@gmail.com>
2026-09-08 01:51:58 +0330

    feat: add signup component

--- frontEnd/src/Components/Auth/Auth.jsx
+++ frontEnd/src/Components/Auth/Auth.jsx
@@ -1,10 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Login from "./elements/login";
+import Signup from "./elements/Signup";
+import Button from "./elements/Button";
 
 function Auth() {
+  const [status, setStatus] = useState("login");
   useEffect(() => {
-    document.title = 'Login | Register'
+    document.title = "Login | `Register`";
   });
+  const changeStatus = (value) => {
+    setStatus(value);
+  };
   return (
     <main className="fixed inset-0 max-sm:px-5 w-full h-full flex items-center justify-center auth-overlay">
       <div className="w-full h-full relative">
@@ -14,25 +20,19 @@ function Auth() {
           alt=""
         />
         <div className="w-full h-full absolute z-10 top-0 auth-overlay"></div>
-        <section className="z-20 absolute top-0 bottom-0 h-fit  m-auto left-0 right-0 px-5 py-5 w-110 bg-input-bg/60 backdrop-blur-xl rounded-xl border border-white/10 auth-form">
+        <section className="z-20 absolute top-0 bottom-0 h-fit  m-auto left-0 right-0 px-5 pt-5 pb-10 w-110 bg-input-bg/60 backdrop-blur-xl rounded-xl border border-white/10 auth-form">
           {/* <!-- Navigations --> */}
           <header className="max-sm:text-xs flex items-center gap-2 w-70 mx-auto text-text-secondary">
-            <button
-              className="w-[50%] py-3 font-semibold border-b-2 border-transparent hover:border-cta-primary hover:text-cta-hover cursor-pointer transition-colors duration-300 auth-header-item auth-header-active"
-              data-status="login"
-            >
+            <Button onSmash={changeStatus} value="login">
               Login
-            </button>
-            <button
-              className="w-[50%] py-3 font-semibold border-b-2 border-transparent hover:border-cta-primary hover:text-cta-hover cursor-pointer transition-colors duration-300 auth-header-item"
-              data-status="signup"
-            >
+            </Button>
+            <Button onSmash={changeStatus} value="signup">
               Sign Up
-            </button>
+            </Button>
           </header>
 
           <main className="mt-10 text-text-primary auth-content">
-            <Login />
+            {status === "login" ? <Login /> : <Signup />}
           </main>
         </section>
       </div>
